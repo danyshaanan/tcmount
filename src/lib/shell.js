ea68f1b8fa92ae84a63f3cb86c6bf987823a4b6f
Dany Shaanan <danyshaanan@gmail.com>
2014-08-06 21:48:19 +0300

    added exit code to shell exit function

--- src/lib/shell.js
+++ src/lib/shell.js
@@ -5,8 +5,8 @@ var spawn = require('child_process').spawn;
 module.exports = function shell(cmd, opts, callback) {
   process.stdin.pause();
   var p = spawn(cmd, opts, { stdio: 'inherit' });
-  return p.on('exit', function() {
+  return p.on('exit', function(code) {
     process.stdin.resume();
-    return callback();
+    return callback(code);
   });
 };

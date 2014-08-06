0416b9e3fa52a6be395a35eabb8e1fdd3766dc47
Dany Shaanan <danyshaanan@gmail.com>
2014-08-06 21:46:42 +0300

    replaced customFds with stdio (same) in spawn

--- src/lib/shell.js
+++ src/lib/shell.js
@@ -4,9 +4,7 @@ var spawn = require('child_process').spawn;
 
 module.exports = function shell(cmd, opts, callback) {
   process.stdin.pause();
-  var p = spawn(cmd, opts, {
-    customFds: [0, 1, 2]
-  });
+  var p = spawn(cmd, opts, { stdio: 'inherit' });
   return p.on('exit', function() {
     process.stdin.resume();
     return callback();

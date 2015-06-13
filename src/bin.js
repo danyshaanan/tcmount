7a109bb88fbc876e9e900a5ce47e9ed2895f6c94
Dany Shaanan <danyshaanan@gmail.com>
2015-06-13 04:57:34 +0300

    fixed broken "tcmount -v"

--- src/bin.js
+++ src/bin.js
@@ -15,7 +15,7 @@ var helpMessage = require('fs').readFileSync(__filename.replace(/\/[^\/]+$/, '')
 
 
 if (cli.targets.length === 2)  qp.freeMount(cli.targets[0], cli.targets[1])
-else if (cli.v)                console.log(require('./package.json').version)
+else if (cli.v)                console.log(require('../package.json').version)
 else if (cli.h)                console.log(helpMessage)
 else if (cli.l || !cli.target) qp.list()
 else if (cli.u)                qp.unmount(cli.target)
